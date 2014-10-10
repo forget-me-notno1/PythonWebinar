3b191aa8ac2ab55538454cf344abb3a980fadead
forget-me-notno1 <mavka.05@mail.ru>
2014-10-10 18:39:36 +0400

    kratnie 5

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import random
 
 def main():
     for i in range(128):
-        print(random.randrange(0, 1024))
+        r = random.randrange(0, 1024)
+        if r % 5 == 0:
+            print(r)
 
 main()
